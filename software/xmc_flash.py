6fb6a9887fdc257483b6212258fdeda8cc670fa8
Olaf Lüke <olaf@tinkerforge.com>
2016-11-08 18:36:08 +0100

    Add comments and better print output

--- software/xmc_flash.py
+++ software/xmc_flash.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
+# Test program for simple bootstrapper/firmware flashing
+# Copy firmware as "fw.bin" in software folder.
+# Configure variables below as necessary
+
+CONFIG_TTY = 115200 # 115200 is maximum for standard bootstrap loader
+CONFIG_PORT = '/dev/ttyACM0'
+CONFIG_FIRMWARE_NAME = 'fw.bin'
+
+
 import serial
 import time
+
 BSL_START  = [0x00]
 BSL_ASC_F  = [0x6C]
 BSL_ASC_H  = [0x12]
@@ -17,12 +27,12 @@ BSL_NOK    = [0x02]
 
 with serial.Serial() as s:
     s = serial.Serial()
-    s.baudrate = 115200 # 115200 is maximum for standard bootstrap loader
-    s.port = '/dev/ttyACM0'
+    s.baudrate = CONFIG_TTY
+    s.port = CONFIG_PORT
     s.timeout = 2.0
     s.open()
 
-    print(s.name)
+    print("Opening " + str(s.name))
 
     s.write(BSL_START)
     s.write(BSL_ASC_F)
@@ -46,7 +56,7 @@ with serial.Serial() as s:
         elif ord(ret) != BSL_OK[0]:
             print("Write bootstrapper length error, received: " + hex(ord(ret)))
         else:
-            print("Wrinting firmware")
+            print("Writing bootstrapper")
             s.write(bs)
             ret = s.read(1)
 
@@ -55,28 +65,28 @@ with serial.Serial() as s:
             elif ord(ret) != BSL_OK[0]:
                 print("Write bootstrapper error, received: " + hex(ord(ret)))
             else:
-                print("Done")
+                print("Writing bootstrapper done")
             
-            s.flush()
+                s.flush()
 
-            fw = open('fw.bin', 'rb').read()
+                fw = open(CONFIG_FIRMWARE_NAME, 'rb').read()
 
-            pages = len(fw)//256
-            for page in range(pages):
-                print("Write firmware page " + str(page))
-                s.write(fw[page*256:(page+1)*256])
-                
-                if page == pages-1: # Last page does not return CRC
-                    break
+                pages = len(fw)//256
+                for page in range(pages):
+                    print("Write firmware page " + str(page))
+                    s.write(fw[page*256:(page+1)*256])
+                    
+                    if page == pages-1: # Last page does not return CRC
+                        break
 
-                ret_crc = s.read(1)
-                crc = 0
-                for i in range(256):
-                    crc = crc ^ fw[page*256+i] 
-                print("Read CRC (mcu vs calc): {0} vs {1}".format(ord(ret_crc), crc))
-                if crc != ord(ret_crc):
-                    print("CRC Error")
-                    break
+                    ret_crc = s.read(1)
+                    crc = 0
+                    for i in range(256):
+                        crc = crc ^ fw[page*256+i] 
+                    print("Read CRC (mcu vs calc): {0} vs {1}".format(ord(ret_crc), crc))
+                    if crc != ord(ret_crc):
+                        print("CRC Error")
+                        break
 
 
             

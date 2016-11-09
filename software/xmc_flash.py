850219e1aa89a5a363ec5f3e4ff32b49f6a251f0
Olaf Lüke <olaf@tinkerforge.com>
2016-11-09 16:40:32 +0100

    put flash functionality in function

--- software/xmc_flash.py
+++ software/xmc_flash.py
@@ -4,9 +4,9 @@
 # Copy firmware as "fw.bin" in software folder.
 # Configure variables below as necessary
 
-CONFIG_TTY = 115200 # 115200 is maximum for standard bootstrap loader
-CONFIG_PORT = '/dev/ttyACM0'
-CONFIG_FIRMWARE_NAME = 'fw.bin'
+CONFIG_BAUDRATE = 19200 #115200 # 115200 is maximum for standard bootstrap loader
+CONFIG_TTY      = '/dev/ttyACM0'
+CONFIG_FIRMWARE = 'fw.bin'
 
 
 import serial
@@ -25,24 +25,25 @@ BSL_BR_OK  = [0xF0]
 BSL_OK     = [0x01]
 BSL_NOK    = [0x02]
 
-with serial.Serial() as s:
-    s = serial.Serial()
-    s.baudrate = CONFIG_TTY
-    s.port = CONFIG_PORT
-    s.timeout = 2.0
-    s.open()
+def xmc_flash(baudrate, tty, firmware):
+    with serial.Serial() as s:
+        s = serial.Serial()
+        s.baudrate = baudrate
+        s.port = tty
+        s.timeout = 0.25
+        s.open()
 
-    print("Opening " + str(s.name))
+        print("Opening " + str(s.name))
 
-    s.write(BSL_START)
-    s.write(BSL_ASC_F)
+        s.write(BSL_START)
+        s.write(BSL_ASC_F)
+        print("start read")
+        ret = s.read(1)
+        if len(ret) < 1:
+            raise Exception("Handshake error: No answer")
+        elif ord(ret) != BSL_ID[0]:
+            raise Exception("Handshake error, received: " + hex(ord(ret)))
 
-    ret = s.read(1)
-    if len(ret) < 1:
-        print("Handshake error: No answer")
-    elif ord(ret) != BSL_ID[0]:
-        print("Handshake error, received: " + hex(ord(ret)))
-    else:
         bs = open('build/bootstrapper.bin', 'rb').read()
         length = len(bs)
         length_write = [length & 0xFF, (length >> 8) & 0xFF, (length >> 16) & 0xFF, (length >> 24) & 0xFF]
@@ -52,41 +53,42 @@ with serial.Serial() as s:
         ret = s.read(1)
 
         if len(ret) < 1:
-            print("Write bootstrapper length error: No answer")
+            raise Exception("Write bootstrapper length error: No answer")
         elif ord(ret) != BSL_OK[0]:
-            print("Write bootstrapper length error, received: " + hex(ord(ret)))
-        else:
-            print("Writing bootstrapper")
-            s.write(bs)
-            ret = s.read(1)
+            raise Exception("Write bootstrapper length error, received: " + hex(ord(ret)))
 
-            if len(ret) < 1:
-                print("Write bootstrapper error: No answer")
-            elif ord(ret) != BSL_OK[0]:
-                print("Write bootstrapper error, received: " + hex(ord(ret)))
-            else:
-                print("Writing bootstrapper done")
+        print("Writing bootstrapper")
+        s.write(bs)
+        ret = s.read(1)
+
+        if len(ret) < 1:
+            raise Exception("Write bootstrapper error: No answer")
+        elif ord(ret) != BSL_OK[0]:
+            raise Exception("Write bootstrapper error, received: " + hex(ord(ret)))
+
+        print("Writing bootstrapper done")
+    
+        s.flush()
+
+        fw = open(firmware, 'rb').read()
+
+        pages = len(fw)//256
+        for page in range(pages):
+            print("Write firmware page " + str(page))
+            s.write(fw[page*256:(page+1)*256])
             
-                s.flush()
+            if page == pages-1: # Last page does not return CRC
+                break
 
-                fw = open(CONFIG_FIRMWARE_NAME, 'rb').read()
+            ret_crc = s.read(1)
+            crc = 0
+            for i in range(256):
+                crc = crc ^ ord(fw[page*256+i])
 
-                pages = len(fw)//256
-                for page in range(pages):
-                    print("Write firmware page " + str(page))
-                    s.write(fw[page*256:(page+1)*256])
-                    
-                    if page == pages-1: # Last page does not return CRC
-                        break
-
-                    ret_crc = s.read(1)
-                    crc = 0
-                    for i in range(256):
-                        crc = crc ^ fw[page*256+i] 
-                    print("Read CRC (mcu vs calc): {0} vs {1}".format(ord(ret_crc), crc))
-                    if crc != ord(ret_crc):
-                        print("CRC Error")
-                        break
+            print("Read CRC (mcu vs calc): {0} vs {1}".format(ord(ret_crc), crc))
+            if crc != ord(ret_crc):
+                raise Exception("CRC Error")
 
 
-            
+if __name__ == '__main__':
+    xmc_flash(CONFIG_BAUDRATE, CONFIG_TTY, CONFIG_FIRMWARE)
